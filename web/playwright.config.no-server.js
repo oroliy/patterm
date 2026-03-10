00e32f7783cc661752e94498f87a38c1721c550a
liy <oroliy@users.noreply.github.com>
2026-03-10 17:44:17 +0800

    fix: let playwright manage web ci server

--- web/playwright.config.no-server.js
+++ web/playwright.config.no-server.js
@@ -18,4 +18,12 @@ export default defineConfig({
             use: { ...devices['Desktop Chrome'] },
         },
     ],
+    webServer: {
+        command: 'npx vite --port 5173',
+        url: 'https://127.0.0.1:5173',
+        timeout: 120 * 1000,
+        reuseExistingServer: !process.env.CI,
+        stdout: 'pipe',
+        stderr: 'pipe',
+    },
 });
